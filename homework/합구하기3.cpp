4aa05495231b7dab9c3b9bed49455ca2b8bb8545
BlackBean99 <ymecca12@gmail.com>
2022-06-10 16:16:58 +0900

    refactor : 합구하기

--- homework/합구하기3.cpp
+++ homework/합구하기3.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 using namespace std;
 
+/**
 // 1부터 인덱스 i 까지의 부분합을 구하는 함수
 int getRangeSumFromOne(int i){
     int sum = 0;
@@ -28,10 +29,27 @@ long long getAnswer(int n){
     return totalSum;
 }
 
+*/
+int getReculsiveSum(int n){
+    if(n==1){
+        return 1;
+    }else{
+        return n += getReculsiveSum(n-1);
+    }
+}
+
+int getAnswer(int end){
+    int sum = 0;
+    
+    for(int i = 1 ; i < end+1; i++){
+        sum += getReculsiveSum(i);
+    }
+    return sum;
+}
 int main(){
     int n;
     scanf("%d", &n);
-    long long answer = getAnswer(n);
+    int answer = getAnswer(n);
     printf("%lld\n",answer);
     return 0;
 }
