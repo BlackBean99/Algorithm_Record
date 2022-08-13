9413f85c991d5d1b12cc4d86a3f4e159cfeaec25
BlackBean99 <ymecca12@gmail.com>
2022-08-14 01:24:12 +0900

    15683

--- BOJ/15683.cpp
+++ BOJ/15683.cpp
@@ -6,135 +6,84 @@ using namespace std;
 
 
 int board[102][102];
-int processedBoard[102][102];
+int board2[102][102];
 
+vector<pair<int,int>> cctv;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1}
 
-int visited[102][102];
 int n, m;
+
+bool OOB(int a, int b){
+    return a<0||a>=n||b<0||b>=m;
+}
+
+void upd(int x, int y, int dir){
+    dir %= 4;
+    while(1){
+        x += dx[dir];
+        y += dy[dir];
+        if(OOB(x,y) || board2[x][y] ==6) return;
+        if(board2[x][y] != 0) continue;
+        board2[x][y] = 7;
+    }
+}
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> n >> m;
 
+
+    int mn = 0;
+
+
+
     queue<pair<int,int>> Q;
     for(int i = 0 ; i <n;i++){
         for(int j = 0 ; j < m;j++){
             cin >> board[i][j];
-            if(board[i][j] != 0 || board[i][j] != 6){
-                Q.push({i,j});
+            if(board[i][j] != 0 && board[i][j] != 6){
+                cctv.push_back({i,j});
             }
+            if(board1[i][j] == 0) mn++;
         }   
     }
 
-    while(!Q.empty()){
-        pair<int,int> cur = Q.front(); Q.pop();
-        
-        for(int dir = 0 ; dir < 4; dir++){
-            // nx 0
-           
-                while(!board[nx][ny] == 6 || !Q.empty()){
-                     if(board[cur.X][cur.Y] ==1){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny <0 || ny >= m)
-                        break;
-                    if(board[nx][ny] == 0)
-                        Q.push({nx,ny});
-                        processedBoard[nx][ny] = -1;
-                    if(board[nx][ny] != 0)
-                        continue;
-                }
+    for(int tmp = 0; tmp < (1<<(2*cctv.size())); tmp++){
+        for(int i = 0 ; i <n;i++)
+            for(int j = 0 ; j < m;j++)
+                board2[i][j] = board1[i][j];
+        int brute = tmp;
+        for(int i = 0; i < cctv.size(); i++){
+            int dir = brute %4;
+            brute /=4;
+            int x = cctv[i].X;
+            int y = cctv[i].Y;
+            if(board1[x][y] == 2){
+                upd(x,y,dir);
+                upd(x,y,dir+2);
             }
-            // nx 0,2
-            if(board[cur.X][cur.Y] ==2){
-                while(!board[nx][ny] == 6){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny <0 || ny >= m)
-                        break;
-                    if(board[nx][ny] == 0)
-                        processedBoard[nx][ny] = -1;
-                        int AdirX = (dir+2)%4;
-                        int AdirY = (dir+2)%4;
-                        processedBoard[AdirX][AdirY] = -1;
-                    if(board[nx][ny] != 0)
-                        continue;
-                }
+            else if(board1[x][y] == 3){
+                upd(x,y,dir);
+                upd(x,y,dir+1);
             }
-            // nx 0,1
-            if(board[cur.X][cur.Y] ==3){
-                while(!board[nx][ny] == 6){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny <0 || ny >= m)
-                        break;
-                    if(board[nx][ny] == 0)
-                        processedBoard[nx][ny] = -1;
-                        int AdirX = (dir+1)%4;
-                        int AdirY = (dir+1)%4;
-                        processedBoard[AdirX][AdirY] = -1;
-                    if(board[nx][ny] != 0)
-                        continue;
-                }
-            }
-            // nx 0,1,2
-            if(board[cur.X][cur.Y] ==4){
-                while(!board[nx][ny] == 6){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny <0 || ny >= m)
-                        break;
-                    if(board[nx][ny] == 0)
-                    // 0
-                        processedBoard[nx][ny] = -1;
-                    // 1
-                        int AdirX = (dir+1)%4;
-                        int AdirY = (dir+1)%4;
-                        processedBoard[AdirX][AdirY] = -1;
-                    // 2
-                        int BdirX = (dir+2)%4;
-                        int BdirY = (dir+2)%4;
-                        processedBoard[BdirX][BdirY] = -1;
-                    if(board[nx][ny] != 0)
-                        continue;
-                }
-            }
-            // nx 0,1,2,3
-            if(board[cur.X][cur.Y] ==5){
-                while(!board[nx][ny] == 6){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    if(nx < 0 || nx >= n || ny <0 || ny >= m)
-                        break;
-                    if(board[nx][ny] == 0)
-                    // 0
-                        processedBoard[nx][ny] = -1;
-                    // 1
-                        int AdirX = (dir+1)%4;
-                        int AdirY = (dir+1)%4;
-                        processedBoard[AdirX][AdirY] = -1;
-                    // 2
-                        int BdirX = (dir+2)%4;
-                        int BdirY = (dir+2)%4;
-                        processedBoard[BdirX][BdirY] = -1;
-                    // 3
-                        int CdirX = (dir+3)%4;
-                        int CdirY = (dir+3)%4;
-                        processedBoard[CdirX][CdirY] = -1;
-                            
-                    if(board[nx][ny] != 0)
-                        continue;
-                }
+            else if(board1[x][y] == 4){
+                upd(x,y,dir);
+                upd(x,y,dir+1);
+                upd(x,y,dir+2);
+            }else{
+                upd(x,y,dir);
+                upd(x,y,dir+1);
+                upd(x,y,dir+1);
+                upd(x,y,dir+3);
             }
         }
+        int val = 0;
+        for(int i = 0 ; i <n;i++)
+            for(int j = 0 ; j < m;j++)
+            val += (board2[i][j] == 0);
+        mn = min(mn,val);
     }
-    int cnt = 0;
-    for(int i = 0;i<m;i++)
-        for(int j = 0;j<m;j++)
-            if(processedBoard[i][j] == -1)
-                cnt++;
-    cout << cnt++;
+    cout << mn;;
 }
