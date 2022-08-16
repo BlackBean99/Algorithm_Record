5d7ce06262e47219fa444c623c0a63589411b23f
BlackBean99 <ymecca12@gmail.com>
2022-08-16 19:14:35 +0900

    2217

--- BOJ/1931.cpp
+++ BOJ/1931.cpp
@@ -1,43 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 
-// 한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다. 
-// 각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자. 
-// 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다. 
-// 회의의 시작시간과 끝나는 시간이 같을 수도 있다. 이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
+int n;
+pair<int, int> s[100005]; // schedule, 정렬의 편의를 위해 {끝 시간, 시작 시간}으로 저장
 
-// 첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다.
-// 둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다. 
-// 시작 시간과 끝나는 시간은 231-1보다 작거나 같은 자연수 또는 0이다.
-
-int main(){
-
-    // cin, cout 속도 향상을 위해 동기화 해제   
-    ios_base::sync_with_stdio(false);
-    // cin.tie(NULL), cout.tie(NULL);
-
-
-    int countLecture;
-    cin >> countLecture;
-    map<int, int> timeTable;
-//  데이터 입력
-    for(int i=0; i<countLecture; i++){
-        int tmp1, tmp2;
-        cin >> tmp1;
-        cin >> tmp2;
-        timeTable[tmp1] = tmp2;
-    }
-
-    int index = 0;
-    int startIndex = 0;
-    int endIndex = 0;
-    map<int, int>::iterator it;
-
-    for(auto it = timeTable.begin(); it!=timeTable.end(); it++){
-        cout << it->first << ", " << it->second << endl;
-    }
-
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 0; i < n; i++)
+    cin >> s[i].second >> s[i].first;
+  sort(s,s+n); // 먼저 끝나는 시간을 비교하고, 끝나는 시간이 동일하면 시작 시간 순으로 정렬
+  int ans = 0;
+  int t = 0; // 현재 시간
+  for(int i = 0; i < n; i++){
+    if(t > s[i].second) continue; // 시작 시간이 현재 시간보다 이전인 회의라면 무시
+    ans++; // 시작 시간이 현재 시간 이후인 회의를 찾았으므로 회의의 수에 1 증가
+    t = s[i].first; // 현재 시간을 s[i]의 끝나는 시간으로 변경한다.
+  }
+  cout << ans;
 }
+
+
+
+//     // cin, cout 속도 향상을 위해 동기화 해제   
+//     ios_base::sync_with_stdio(false);
+//     // cin.tie(NULL), cout.tie(NULL);
+
+
+//     int countLecture;
+//     cin >> countLecture;
+//     map<int, int> timeTable;
+// //  데이터 입력
+//     for(int i=0; i<countLecture; i++){
+//         int tmp1, tmp2;
+//         cin >> tmp1;
+//         cin >> tmp2;
+//         timeTable[tmp1] = tmp2;
+//     }
+
+//     int index = 0;
+//     int startIndex = 0;
+//     int endIndex = 0;
+//     map<int, int>::iterator it;
+
+//     for(auto it = timeTable.begin(); it!=timeTable.end(); it++){
+//         cout << it->first << ", " << it->second << endl;
+//     }
+
+// }
