86b2ecd7945b7f18ace85f7115dd158dc7d67f6f
= <ymecca730135@gmail.com>
2022-09-04 22:01:11 +0900

    9205

---
+++ src/BOJ/Main_9205.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+public class Main_9205 {
+    public static int n;
+
+
+    private static int solution(){
+        return 0;
+    }
+    public void main(String[] args){
+        System.out.println("remote");
+
+    }
+}
