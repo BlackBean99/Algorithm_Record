e2222e7b0afcf6eb60a6b1584ad864e87d83c8a1
이서현 <ymecca12@gmail.com>
2023-05-03 21:24:12 +0900

    dp : 효율적인화폐구성

---
+++ python_solve/dp/효율적인화폐구성.py
@@ -0,0 +1,20 @@
+n,m = map(int,input().split())
+array = []
+for i in range(n):
+    array.append(int(input()))
+# m원을 만들 수 있는 모든 경우의 수는?
+
+dp = [10001] * 10000
+# i원을 만들 수 있는 돈의 방법은?
+# i원 - array[i] 방법들을 추가해보면 어떨까?
+
+# 0 부터 m포함해서
+dp[0] = 0
+for i in range(n):
+    for j in range(array[i],m+1):
+        if dp[j- array[i]] != 10001:
+            dp[j] = min(dp[j-array[i]] + 1, dp[j])
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
