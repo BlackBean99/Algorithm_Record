a0453cd8d87bcc5c2246090abbc28bac38972b1b
이서현 <ymecca12@gmail.com>
2023-05-23 11:04:08 +0900

    implementation : 럭키 스트레이트

--- python_solve/greedy/무지의먹방라이브.py
+++ python_solve/greedy/무지의먹방라이브.py
@@ -1,23 +1,25 @@
 import heapq
+
 def solution(food_times, k):
-    #  전체 음식을 먹는 시간보다 k 가 크거나 같다면 -1
-    if sum(food_times) >= k:
+    if sum(food_times) <= k:
         return -1
-    # 시간이 적은 음식부터 빼야하므로 우선순위 큐를 이용
+
+    # 시간이 적은 순서대로 삽입해야 하므로 우선순위 큐를 사용한다
     q = []
     for i in range(len(food_times)):
         heapq.heappush(q,(food_times[i], i+1))
 
-    sum_value = 0 # 먹기 전에 사용한 시간
-    previous = 0 # 직전에 다 먹은 음식 시간
+    # 지금까지 먹은 음식 시간
+    sum_value =0
+    # 이전 음식까지 먹은 시간
+    previoud = 0
 
     length = len(food_times)
-    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 K 비교
-    while sum_value + ((q[0][0] - previous)*length) <= k:
-        now = heapq.heappop()
-        sum_value = (now - previous) * length
-        length -= 1 # 다 먹은 음식 제외
-        previous = now # 이전 음식 재설정
-    # 남은 음식중에서 몇번째 음식인지 확인하면서 출력
+    while sum_value + ((q[0][0] - previoud) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previoud) * length
+        length -= 1
+        previoud = now
+    # 음식의 번호 기준으로 정렬
     result = sorted(q, key = lambda x: x[1])
-    return result[(k-sum_value) % length][1]
+    return result[(k - sum_value) % length]
