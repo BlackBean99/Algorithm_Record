f4c7ecebda717949da04ed624d8e0e92af845c84
이서현 <ymecca12@gmail.com>
2023-05-26 19:29:54 +0900

    implementation : 외벽점검

--- python_solve/implementation/치킨배달.py
+++ python_solve/implementation/치킨배달.py
@@ -1,34 +1,29 @@
 from itertools import combinations
 
-n,m = map(int,input().split())
+n,m = map(int,input())
 chicken, house = [], []
 
 for r in range(n):
-    data  = list(map(int,input().split()))
+    data = list(map(int,input().split()))
     for c in range(n):
+        # 일반 집인 경우
         if data[c] == 1:
-            house.append(((r,c)))
-        elif data[c] == 2:
-            chicken.append((r,c))
-# 모든 치킨집 중에서 m개의 치킨집을 뽑는 조합 계산
+            house.append((r, c))
+        if data[c] == 2:
+            chicken.append((r, c))
+
 candidates = list(combinations(chicken,m))
 
 def get_sum(candidate):
     result = 0
-    # 모든 집에 대하여
-    for hx, hy in house:
-        # 가장 가까운 치킨집을 찾기
+    #     모든 집에 대해서
+    for hx,hy in house:
+    # 모든 치킨집을
         temp = 1e9
         for cx,cy in candidate:
-            temp = min(temp,abs(hx-cx) + abs(hy-cy))
-        result += temp
-    return result
-
+            temp = min(temp,abs(cx-hx)+abs(cy-hy))
+        #가장 가까운
 result = 1e9
 for candidate in candidates:
-    result = min(result, get_sum(candidate))
-
+    result = min(result,get_sum(candidate))
 print(result)
-#  집의 위치를 저장한 배열
-#  치킨집의 위치를 저장한 배열
-
