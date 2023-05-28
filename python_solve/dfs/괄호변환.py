61dfae91d67d7a258ac34e3dedf70e3d3a16d9d4
이서현 <ymecca12@gmail.com>
2023-05-28 22:35:07 +0900

    괄호변환

--- python_solve/dfs/괄호변환.py
+++ python_solve/dfs/괄호변환.py
@@ -1,45 +1,45 @@
-def balanced_index(p):
-    count = 0
+def divide(p):
+    open_p = 0
+    close_p = 0
+
     for i in range(len(p)):
         if p[i] == '(':
-            count += 1
+            open_p += 1
         else:
-            count -= 1
-        if count == 0:
-            return i
+            close_p += 1
+        if open_p == close_p:
+            # 닫힌 괄호가 다 짝이 맞았으먄 그 이전과 이후 배열로 구분한다
+            return p[:i+1], p[i+1:]
 
-def check_proper(p):
-    count = 0
-    for i in p:
-        if i == '(':
-            count += 1
+def check(u):
+    stack = []
+    for p in u:
+        if p == '(':
+            stack.append(p)
         else:
-            if count == 0:
+            if not stack:
                 return False
-        count -= 1
+            stack.pop()
     return True
 
-
 def solution(p):
-    answer = ''
-    if p == '':
-        return answer
-    index = balanced_index(p)
-    u = p[:index+1]
-    v = p[index+1:]
-    if check_proper(u):
-        answer = u + solution(v)
+    if not p:
+        return ""
+    u,v = divide(p)
+    if check(u):
+        # 3.문자열u가 "올바른 괄호 문자열"이라면문자열v에대해1단계부터다시수행합니다.
+        # 3 - 1.수행한 결과 문자열을 u 에 이어붙인 후 반환 합니다.
+        return u + solution(v)
     else:
         answer = '('
         answer += solution(v)
         answer += ')'
 
-        u = list(u[1:-1])
-        for i in range(len(u)):
-            if u[i] == '(':
-                u[i] = ')'
-            else:
-                u[i] = '('
-        answer += "".join(u)
+    # 처음걸 떄고
+    for p in u[1:len(u)-1]:
+        if p == '(':
+            answer += ')'
+        else:
+            answer += '('
     return answer
-print(solution("((()()())"))
+print(solution("(()())()"))
