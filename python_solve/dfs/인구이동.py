9c2bf127c17abfc031c2e189f69892bfbffb0857
이서현 <ymecca12@gmail.com>
2023-06-02 16:27:04 +0900

    style : dfs queue 형식으로 인구이동 변경

--- python_solve/dfs/인구이동.py
+++ python_solve/dfs/인구이동.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n,l,r = map(int,input().split())
 
 graph = []
@@ -9,19 +11,44 @@ for i in range(n):
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 
-count = 0
+result = 0
 
-x,y = 0,0
-for i in range(n):
-    for j in range(n):
-        x, y = i,j
+def process(x,y,index):
+    # (x,y)의 위치와 연결된 연합 정보를 담는 리스트
+    united = []
+    united.append((x,y))
+
+    q = deque()
+    q.append((x,y))
+    union[x][y] = index
+    summary = graph[x][y] # 현재 연합의 번호 할당
+    count = 1 # 현재 연합의 국가 수
+    while q:
+        x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
-            ny = y +dy[i]
-            if nx <= 0 and nx < n and ny <= 0 and ny < n:
-                if abs(graph[x][y] - graph[nx][ny]) >= l and abs(graph[x][y] - graph[nx][ny]) <= r:
-                    graph[x][y] = (graph[x][y] + graph[nx][ny]) // 2
-                    graph[nx][ny] = (graph[x][y] + graph[nx][ny]) // 2
+            ny = y + dy[i]
+            if 0 <= nx and nx < n and 0 <= ny and ny < n and union[nx][ny] == -1:
+                if 1<= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    q.append((nx,ny))
+                    union[nx][ny]  = index
+                    summary += graph[nx][ny]
                     count += 1
+                    united.append((nx,ny))
+    for i, j in united:
+        graph[i][j] = summary // count
+    return count
 
-print(count)
+total_count = 0
+
+while True:
+    union = [[-1] * n for _ in range(n)]
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            if union[i][j] == -1:
+                process(i,j,index)
+                index += 1
+    if index == n*n:
+        break
+    total_count += 1
