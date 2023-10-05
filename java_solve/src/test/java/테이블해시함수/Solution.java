1a280a3286edd0a33f535df343b7fdccb11f515c
이서현 <ymecca12@gmail.com>
2023-10-05 18:24:37 +0900

    [solve] : 테이블해시함수

--- java_solve/src/test/java/테이블해시함수/Solution.java
+++ java_solve/src/test/java/테이블해시함수/Solution.java
@@ -1,5 +1,7 @@
 package 테이블해시함수;
 
+import java.util.Arrays;
+
 class Solution {
     public static void main(String[] args) {
         int[][] data1 = new int[][]{{2, 2, 6}, {1, 5, 10}, {4, 2, 9}, {3, 8, 3}};
@@ -22,8 +24,31 @@ class Solution {
         else throw new RuntimeException(sb.toString());
     }
 
-    public int solution(int[][] data, int col, int row_begin, int row_end) {
+    public static int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
+
+        col -= 1;
+        row_begin -= 1;
+
+        // 2. 정렬
+        int finalCol = col;
+        Arrays.sort(data, (o1, o2) -> {
+            if (o1[finalCol] == o2[finalCol]) return o2[0] - o1[0];
+            return o1[finalCol] - o2[finalCol];
+        });
+
+        // 3. S_i 합 구하기
+        for (int i = row_begin; i < row_end; i++) {
+
+            int finalI = i + 1;
+            int dataTotal = Arrays.stream(data[i])
+                    .map(j -> j % finalI)
+                    .sum();
+
+            // 4. XOR한 값 저장
+            answer = (answer ^ dataTotal);
+        }
+
         return answer;
     }
 }
