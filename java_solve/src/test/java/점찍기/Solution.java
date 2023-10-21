bf152cf715b02487aca58d5e0160b32bcd116bf8
이서현 <ymecca12@gmail.com>
2023-10-21 23:55:43 +0900

    [develop] : 점찍기 개선

--- java_solve/src/test/java/점찍기/Solution.java
+++ java_solve/src/test/java/점찍기/Solution.java
@@ -27,25 +27,14 @@ class Solution {
     }
 
     public long solution(int k, int d) {
-//        0,0부터
         long answer = 0;
-//          k * d(i = 0 ; i <d; i*k < d)
-
-//         1,5
-//         0,1(0 + 1*1),2(0 + 1*2),3(0+1*3),4(0 + 1*4),5(0 + 1*5)
-//         0,1(0 + 1*1),2(0 + 1*2),3(0+1*3),4(0 + 1*4),5(0 + 1*5)
-//         d <
-        int startX = 0;
-        int startY = 0;
-        for (int i = 0; i <= d / k; i++) {
-            for (int j = 0; j <= d / k; j++) {
-                int endX = startX + i * k;
-                int endY = startY + j * k;
-                if (Math.sqrt(endX * endX + endY * endY) <= d) {
-                    answer += 1;
-                }
-            }
+        for(int i = 0; i <= d; i += k)
+        {
+            long dd = (long) d * d;
+            long ii = (long) i * i;
+            int top = (int) Math.sqrt(dd - ii);
+            answer += (top / k) + 1;
         }
         return answer;
-    }
+        }
 }
