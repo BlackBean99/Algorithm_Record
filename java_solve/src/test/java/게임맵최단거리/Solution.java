42212614a2a099e3931ce6a33cb512775ac859d5
이서현 <ymecca12@gmail.com>
2023-10-25 15:27:05 +0900

    [build] : 게임맵최단거리

---
+++ java_solve/src/test/java/게임맵최단거리/Solution.java
@@ -0,0 +1,31 @@
+package 게임맵최단거리;
+
+class Solution {
+    public static void main(String[] args) {
+        int[][] maps1 = new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
+        int answer1 = 11;
+        int result1 = new Solution().solution(maps1);
+        PRINT_RESULT(1, result1, answer1);
+
+        int[][] maps2 = new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}};
+        int answer2 = -1;
+        int result2 = new Solution().solution(maps2);
+        PRINT_RESULT(2, result2, answer2);
+    }
+
+    public static void PRINT_RESULT(int index, int result, int answer) {
+        boolean correct = result == answer;
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n테스트 케이스 ").append(index).append(": ");
+        sb.append(correct ? "정답" : "오답").append("\n");
+        sb.append("\t- 실행 결과: \t").append(result).append("\n");
+        sb.append("\t- 기댓값: \t").append(answer).append("\n");
+        if (correct) System.out.println(sb);
+        else throw new RuntimeException(sb.toString());
+    }
+
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return answer;
+    }
+}
