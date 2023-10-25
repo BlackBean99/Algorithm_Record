6ade32b5ee377420c7cb7d627a8a67366c256523
이서현 <ymecca12@gmail.com>
2023-10-25 16:01:12 +0900

    [solve] : 게임맵최단거리

--- java_solve/src/test/java/게임맵최단거리/Solution.java
+++ java_solve/src/test/java/게임맵최단거리/Solution.java
@@ -1,5 +1,9 @@
 package 게임맵최단거리;
 
+import java.awt.Point;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public static void main(String[] args) {
         int[][] maps1 = new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
@@ -24,8 +28,44 @@ class Solution {
         else throw new RuntimeException(sb.toString());
     }
 
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
     public int solution(int[][] maps) {
-        int answer = 0;
-        return answer;
+        int n = maps.length;
+        int m = maps[0].length;
+
+
+        int[][] visited = new int[n][m];
+        visited[0][0] = 1;
+
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0, 0, 1));
+
+        while(!q.isEmpty()){
+            Point current = q.poll();
+            if(current.x == m-1 && current.y == n-1) return current.distance;
+            for (int dir = 0; dir < 4; dir++) {
+                int nextX = current.x + dx[dir];
+                int nextY = current.y + dy[dir];
+                if (0 <= nextX && nextX < m && 0 <= nextY && nextY < n && maps[nextX][nextY] == 1 && visited[nextX][nextY] == 0) {
+                    q.add(new Point(nextX, nextY, current.distance + 1));
+                    visited[nextX][nextY] = 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+    class Point{
+        int x;
+        int y;
+        int distance;
+
+        public Point(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
     }
 }
