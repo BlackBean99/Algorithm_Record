ebf3e5f198d8959e5f50811517e05a22544648d5
이서현 <ymecca12@gmail.com>
2023-10-26 17:27:08 +0900

    [solve] : 전력망을 둘로 나누기

--- java_solve/src/test/java/전력망을둘로나누기/Solution.java
+++ java_solve/src/test/java/전력망을둘로나누기/Solution.java
@@ -1,5 +1,8 @@
 package 전력망을둘로나누기;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public static void main(String[] args) {
         int n1 = 9;
@@ -32,8 +35,73 @@ class Solution {
         else throw new RuntimeException(sb.toString());
     }
 
+    static int[][] arr;
+
     public int solution(int n, int[][] wires) {
-        int answer = -1;
+        int answer = n;
+        arr = new int[n+1][n+1];
+        for (int i = 0; i < wires.length; i++) {
+            arr[wires[i][0]][wires[i][1]] = 1;
+            arr[wires[i][1]][wires[i][0]] = 1;
+        }
+
+        int a, b;
+        for (int i = 0; i < wires.length; i++) {
+
+            a = wires[i][0];
+            b = wires[i][1];
+
+            arr[a][b] = 0;
+            arr[b][a] = 0;
+
+            answer = Math.min(answer, bfs(n, a));
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
         return answer;
     }
+
+//        return answer;
+
+        private int bfs(int n, int start){
+            int[] visit = new int[n + 1];
+            int cnt = 1;
+
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(start);
+
+            while (!q.isEmpty()) {
+                int point = q.poll();
+                visit[point] = 1;
+                for (int i = 1; i < n + 1; i++) {
+                    if (visit[i] == 1) continue;
+                    if (arr[point][i] == 1) {
+                        q.offer(i);
+                        cnt++;
+                    }
+                }
+            }
+            // 하나 끊으면 cnt, n- cnt 로 나눠질 것이다.
+            // 두 차이를 구하면 n-2cnt 이다.
+            return (int) Math.abs(n - 2 * cnt);
+        }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
