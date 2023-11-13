60deaef465e342ee3c22694fb005ccdb8a3ec12e
이서현 <ymecca12@gmail.com>
2023-11-13 22:55:38 +0900

    [build] : 순위검색

---
+++ java_solve/src/test/java/순위검색/Solution.java
@@ -0,0 +1,29 @@
+package 순위검색;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        String[] info1 = new String[]{"java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"};
+        String[] query1 = new String[]{"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"};
+        int[] answer1 = new int[]{1, 1, 1, 1, 2, 4};
+        int[] result1 = new Solution().solution(info1, query1);
+        PRINT_RESULT(1, result1, answer1);
+    }
+
+    public static void PRINT_RESULT(int index, int[] result, int[] answer) {
+        boolean correct = Arrays.equals(result, answer);
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n테스트 케이스 ").append(index).append(": ");
+        sb.append(correct ? "정답" : "오답").append("\n");
+        sb.append("\t- 실행 결과: \t").append(Arrays.toString(result)).append("\n");
+        sb.append("\t- 기댓값: \t").append(Arrays.toString(answer)).append("\n");
+        if (correct) System.out.println(sb);
+        else throw new RuntimeException(sb.toString());
+    }
+
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+}
