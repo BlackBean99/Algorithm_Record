c9954e2f731cea2b38787b2703def51804e8290b
blackbean <ymecca730135@gmail.com>
2023-12-29 21:56:34 +0900

    [solve] : k진수에서소수개수구하기

--- java_solve/src/test/java/k진수에서소수개수구하기/Solution.java
+++ java_solve/src/test/java/k진수에서소수개수구하기/Solution.java
@@ -27,7 +27,26 @@ class Solution {
     }
 
     public int solution(int n, int k) {
-        int answer = -1;
+        int answer = 0;
+        String temp = "";
+        while (n != 0) {
+            temp = n%k + temp;
+            n /= k;
+        }
+        String[] arr = temp.split("0");
+        for(String s : arr){
+            if(s.equals("")) continue;
+            long num = Long.parseLong(s);
+            if(isPrime(num)) answer++;
+        }
         return answer;
+    }
+
+    private boolean isPrime(long num) {
+        if(num < 2) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 }
