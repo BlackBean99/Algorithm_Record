5f74c086f81b27d44dc15d9ae621b73af49d33f2
blackbean <ymecca730135@gmail.com>
2024-01-11 11:35:16 +0900

    [build]: 오픈채팅방

---
+++ java_solve/src/test/java/오픈채팅방/Solution.java
@@ -0,0 +1,28 @@
+package 오픈채팅방;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        String[] record1 = new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
+        String[] answer1 = new String[]{"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."};
+        String[] result1 = new Solution().solution(record1);
+        PRINT_RESULT(1, result1, answer1);
+    }
+
+    public static void PRINT_RESULT(int index, String[] result, String[] answer) {
+        boolean correct = Arrays.equals(result, answer);
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n테스트 케이스 ").append(index).append(": ");
+        sb.append(correct ? "정답" : "오답").append("\n");
+        sb.append("\t- 실행 결과: \t").append(Arrays.toString(result)).append("\n");
+        sb.append("\t- 기댓값: \t").append(Arrays.toString(answer)).append("\n");
+        if (correct) System.out.println(sb);
+        else throw new RuntimeException(sb.toString());
+    }
+
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
