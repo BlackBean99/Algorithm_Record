ab64cc7e20c2b96f30c6a38ea39940bbab4f2cca
blackbean <ymecca730135@gmail.com>
2024-02-02 23:57:12 +0900

    [build]: 석유시추

---
+++ java_solve/src/test/java/PCCP기출문제2번석유시추/Solution.java
@@ -0,0 +1,31 @@
+package PCCP기출문제2번석유시추;
+
+class Solution {
+    public static void main(String[] args) {
+        int[][] land1 = new int[][]{{0, 0, 0, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 1, 1, 0, 0}, {1, 1, 0, 0, 0, 1, 1, 0}, {1, 1, 1, 0, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0, 1, 1}};
+        int answer1 = 9;
+        int result1 = new Solution().solution(land1);
+        PRINT_RESULT(1, result1, answer1);
+
+        int[][] land2 = new int[][]{{1, 0, 1, 0, 1, 1}, {1, 0, 1, 0, 0, 0}, {1, 0, 1, 0, 0, 1}, {1, 0, 0, 1, 0, 0}, {1, 0, 0, 1, 0, 1}, {1, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1}};
+        int answer2 = 16;
+        int result2 = new Solution().solution(land2);
+        PRINT_RESULT(2, result2, answer2);
+    }
+
+    public static void PRINT_RESULT(int index, int result, int answer) {
+        boolean correct = result == answer;
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n테스트 케이스 ").append(index).append(": ");
+        sb.append(correct ? "정답" : "오답").append("\n");
+        sb.append("\t- 실행 결과: \t").append(result).append("\n");
+        sb.append("\t- 기댓값: \t").append(answer).append("\n");
+        if (correct) System.out.println(sb);
+        else throw new RuntimeException(sb.toString());
+    }
+
+    public int solution(int[][] land) {
+        int answer = 0;
+        return answer;
+    }
+}
