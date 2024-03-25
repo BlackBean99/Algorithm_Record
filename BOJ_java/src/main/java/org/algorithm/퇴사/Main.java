8915edf1205396dca6ddbd8478f235471fbc0b85
blackbean <ymecca730135@gmail.com>
2024-03-25 11:09:35 +0900

    [solve]: 퇴사 dfs

--- BOJ_java/src/main/java/org/algorithm/퇴사/Main.java
+++ BOJ_java/src/main/java/org/algorithm/퇴사/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     static int n;
     static int[][] manage;
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,19 +17,28 @@ public class Main {
 
         StringTokenizer st;
         manage = new int[n][2];
-        int[] dp = new int[n + 1];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             manage[i][0] = Integer.parseInt(st.nextToken());
             manage[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            if (i + manage[i][0] <= n) {
-                dp[i + manage[i][0]] = Math.max(dp[i + manage[i][0]], dp[i] + manage[i][1]);
-            }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        dfs(0, 0);
+        System.out.println(result);
+    }
+
+    static void dfs(int idx, int pay) {
+        if (idx >= n) {
+            result = Math.max(pay, result);
+            return;
         }
-        System.out.println(dp[n]);
+
+        if (idx + manage[idx][0] <= n) { // 상담을 끝마칠 수 있다면 -> 상담이 끝난 날짜와 상담비 넣음
+            dfs(idx + manage[idx][0], pay + manage[idx][1]);
+        } else {
+            dfs(idx + manage[idx][0], pay);
+        }
+        dfs(idx + 1, pay);
     }
 }
+
