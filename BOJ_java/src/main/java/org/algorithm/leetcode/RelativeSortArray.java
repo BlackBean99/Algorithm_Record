73527944c2157059de6137e71492a404b620db99
blackbean99 <ymecca730135@gmail.com>
2024-06-12 22:29:07 +0900

    [refactor]: leet 1122

--- BOJ_java/src/main/java/org/algorithm/leetcode/RelativeSortArray.java
+++ BOJ_java/src/main/java/org/algorithm/leetcode/RelativeSortArray.java
@@ -1,6 +1,7 @@
 package org.algorithm.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,38 +10,49 @@ public class RelativeSortArray {
     public static void main(String[] args) {
         int[] arr1 = new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
         int[] arr2 = new int[]{2, 1, 4, 3, 9, 6};
-        Integer[] integers = relativeSortArray(arr1, arr2);
+        int[] integers = relativeSortArray(arr1, arr2);
         System.out.println(Arrays.toString(integers));
         arr1 = new int[]{28,6,22,8,44,17};
         arr2 = new int[]{22,28,8,6};
         integers = relativeSortArray(arr1, arr2);
         System.out.println(Arrays.toString(integers));
     }
-    public static Integer[] relativeSortArray(int[] arr1, int[] arr2) {
-        List<Integer> temp = new LinkedList<>();
-        List<Integer> answer = new LinkedList<>();
-        boolean[] visited = new boolean[arr1.length + 1];
-
-        for(int i = 0; i < arr2.length; i ++){
-            for(int j = 0; j < arr1.length; j++){
-                if (visited[j]) {
-                    continue;
-                }
-                if(arr2[i] == arr1[j]){
-                    answer.add(arr1[j]);
-                    visited[j] = true;
-                }
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for (int j : arr1) {
+            if (hm.containsKey(j)) {
+                hm.put(j, hm.get(j) + 1);
+            } else {
+                hm.put(j, 1);
             }
         }
-        for (int i = 0; i < visited.length-1; i++) {
-            if(visited[i] == false){
-                temp.add(arr1[i]);
+        int[] ans = new int[arr1.length];
+        int cnt=0;
+        for (int j : arr2) {
+            while (hm.get(j) > 0) {
+                ans[cnt] = j;
+                cnt++;
+                hm.put(j, hm.get(j) - 1);
+            }
+            hm.remove(j);
+        }
+        int[] remaining = new int[arr1.length-cnt];
+        int index = 0;
+        for (int j : arr1) {
+            if (hm.containsKey(j)) {
+                while (hm.get(j) > 0) {
+                    remaining[index] = j;
+                    index++;
+                    hm.put(j, hm.get(j) - 1);
+                }
+                hm.remove(j);
             }
         }
-        temp.sort((a, b) -> a - b);
-        answer.addAll(temp);
-        Integer[] array = answer.toArray(new Integer[answer.size()]);
-
-        return array;
+        Arrays.sort(remaining);
+        for (int j : remaining) {
+            ans[cnt] = j;
+            cnt++;
+        }
+        return ans;
     }
 }
