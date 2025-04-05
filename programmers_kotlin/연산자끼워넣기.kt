f512891d786b01284c0011034952555b8878a304
blackbean99 <ymecca730135@gmail.com>
2025-04-05 23:52:16 +0900

    [backtracking]: 경사로

--- programmers_kotlin/연산자끼워넣기.kt
+++ programmers_kotlin/연산자끼워넣기.kt
@@ -1,66 +1,60 @@
-import java.util.*
-import java.io.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-private val numList: MutableList<Int> = mutableList()
-private val opList: MutableList<Int> = mutableList()
-private val seqOp: MutableList<Int> = mutableList()
+private var n = 0
+private val numList : MutableList<Int> = mutableListOf()
+private val opList: MutableList<Int> = mutableListOf()
+private val seqOp:  MutableList<Int> = mutableListOf()
 private lateinit var visited : BooleanArray
 private var minResult = Integer.MAX_VALUE
 private var maxResult = Integer.MIN_VALUE
 
-
-fun main() {
+fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val n = br.readLine().toInt()
-
+    n = br.readLine().toInt()
     numList.addAll(br.readLine().split(" ").map{ it.toInt() })
-    visited = BooleanArray(n-1)
-    
-    br.readLine.split(" ").map{ it.toInt() }.forEachIndexed { index, it  -> 
+    visited = BooleanArray(n - 1)
+    br.readLine().split(" ").map{ it.toInt()}.forEachIndexed { index, it ->
         repeat(it){
-            opList.add(index)
+            opList.add(index) // 인덱스를 저장(연산자니까)를 여러개
         }
-    }
-    // 카운트를 0으로 만들고
-    // 시뮬레이션
-    operate(n-1)
-    print(maxResult)
+    } // 덧셈, 뺄셈, 곱셈, 나눗셈의 개수
+    operate(n - 1)
+    println(maxResult)
     print(minResult)
 }
 
-
-fun operate(count: Int){
+// 순열 백트래킹
+fun operate(count : Int){
     var result = numList[0]
     if(count == 0){
-        repeat(n-1){ i ->
+        repeat(n - 1){ i ->
             when(seqOp[i]){
                 0 -> {
-                    result += numList[i+1]
+                    result += numList[i + 1]
                 }
                 1 -> {
-                    result -= numList[i+1]
+                    result -= numList[i + 1]
                 }
                 2 -> {
-                    result *= numList[i+1]
+                    result *= numList[i + 1]
                 }
                 3 -> {
-                    result /= numList[i+1]
+                    result /= numList[i + 1]
                 }
             }
         }
         minResult = minOf(minResult, result)
         maxResult = maxOf(maxResult, result)
-        return 
+        return
     }
-
-    for(i in until n-1){
+    for(i in 0 until n - 1){
         if(!visited[i]) {
             seqOp.add(opList[i])
-            visited[i]= true
+            visited[i] = true
             operate(count - 1)
             seqOp.removeLast()
             visited[i] = false
         }
     }
-
 }
