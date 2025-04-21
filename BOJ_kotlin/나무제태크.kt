60f76a06f65e2cdb38ef62cf354a05d4ccfbf62f
blackbean99 <ymecca730135@gmail.com>
2025-04-21 19:53:17 +0900

    [backtracking]: 나무제태크 리펙토링

--- BOJ_kotlin/나무제태크.kt
+++ BOJ_kotlin/나무제태크.kt
@@ -1,74 +1,88 @@
-val dx = intArrayOf(1,1,0,-1,-1,-1,0,1)
-val dy = intArrayOf(0,1,1,1,0,-1,-1,-1)
+import java.util.*
 
-fun main() = with(System.`in`.BufferedReader){
-    val answer = 0
-    val (n,m,k) = readLine().split(" ").map{ it.toInt() }
-    val injection = Array(n){ IntArray(5) }
-    val map = Array(n){ IntArray(5) {5} }
-    for(i in 0 until n){
-        val arr: IntArray = readLine().split(" ").map{ it.toInt() }
-        injection[i] = arr
+lateinit var nut: Array<IntArray>
+lateinit var ground: Array<IntArray>
+var trees = PriorityQueue<Tree>()
+val dead: Queue<Tree> = LinkedList()
+val dr = listOf(-1, 0, 1, 0, -1, -1, 1, 1)
+val dc = listOf(0, 1, 0, -1, -1, 1, -1, 1)
+fun main() {
+    /**
+     * N: 땅의 한 변의 크기
+     * M: 나무의 개수
+     * K: 목표 년도
+     */
+
+    val (N, M, K) = readln().split(" ").map { it.toInt() }
+    nut = Array(N) { IntArray(N) }
+    ground = Array(N) { IntArray(N) { 5 } }
+    for (i in nut.indices) {
+        nut[i] = readln().split(" ").map { it.toInt() }.toIntArray()
     }
-    
-    // x,y 별로 
-    val trees = Array(n) { Array(n) { LinkedList<Int>> }}
-    val deadTrees = Array(n) { Array(n) { LinkedList<Int>> }}
-    for(i in 0 until m){
-        val (x,y,z) = readLine().split(" ").map{ it.toInt() }
-        trees[x][y].add(z)
+
+    for (i in 0 until M) {
+        val (r, c, lev) = readln().split(" ").map { it.toInt() }
+        trees.offer(Tree(r - 1, c - 1, lev))
     }
+    var year = 1
+    while (year <= K) {
+        // 봄 : 나무들이 양분을 먹고 자라거나, 죽는다.
+        spring()
+        // 여름 : 죽은 나무가 양분으로 변한다.
+        summer()
+        // 가을 : 레벨이 5의 배수인 나무가 번식한다.
+        autumn()
+        // 겨울 : S2D2가 땅을 돌아다니며 양분을 추가한다.
+        winter()
 
-    // 전체 나무의 수 = 0
-    var treeCount = 0
-    // k 년 만큼 반복한다.
-    for(age in 0 until k){
-    // 봄 : 나이가 어린 나무부터 나이만큼 쳐먹는다 -> 자신의 나이만큼 못먹은 애들은 죽는 애들 리스트로 추가한다.
-        for(i in 0 until n){
-            for(j in 0 until n){
-                trees[i][j].sortedWith(compareBy({it}))
-                for(target in trees[i][j].indices){
-                    if(map[i][j] >= trees[i][j][target]){
-                        trees[i][j][target]++
-                    } else {
-                        deadTrees[i][j].add(trees[i][j].poll())
-                    }
-                }
+        year++
+    }
+    println(trees.size)
+}
 
-                // 여름 : 죽은 나무들 list -> 나무들의 third / 2 를 더한다. += poll / 2
-                for(target in deadTrees[i][j].indices){
-                    map[i][j] += deadTrees[i][j].poll() / 2
-                }
-                // 가을 : 8개의 dx dy에 나이가 1인 나무를 생성한다.  trees.add 8개 한다 inbound내에서
-                for(target in trees[i][j].indices){
-                    if(trees[i][j][target] % 5 == 0){
-                        for(dir in dx.indices){
-                            val nx = i + dx[dir]
-                            val ny = j + dy[dir]
-                            if(isInBound(nx,ny)){
-                                trees[nx][ny].add(1)
-                            }
-                        }
-                    }
+fun winter() {
+    for (i in ground.indices) {
+        for (j in ground.indices) {
+            ground[i][j] += nut[i][j]
+        }
+    }
+}
+fun autumn() {
+    val survive = PriorityQueue(trees)
+    while (trees.isNotEmpty()) {
+        val tree = trees.poll()
+        if (tree.lev % 5 == 0) {
+            for (i in dr.indices) {
+                val nr = tree.r + dr[i]
+                val nc = tree.c + dc[i]
+                if (inRange(nr, nc)) {
+                    survive.offer(Tree(nr, nc, 1))
                 }
             }
         }
-        // 겨울 : 땅에 양분을 추가한다
-        for(i in 0 until n){
-            for(j in 0 until n){
-                map[i][j] += injection[i][j]
-            }
+    }
+    trees = PriorityQueue(survive)
+}
+fun inRange(r: Int, c: Int): Boolean = r in ground.indices && c in ground.indices
+fun summer() {
+    while (dead.isNotEmpty()) {
+        val deadTree = dead.poll()
+        ground[deadTree.r][deadTree.c] += deadTree.lev / 2
+    }
+}
+fun spring() {
+    val survive = PriorityQueue<Tree>()
+    while (trees.isNotEmpty()) {
+        val tree = trees.poll()
+        if (tree.lev <= ground[tree.r][tree.c]) {
+            survive.offer(Tree(tree.r, tree.c, tree.lev + 1))
+            ground[tree.r][tree.c] -= tree.lev
+        } else {
+            dead.offer(tree)
         }
     }
-    // tree의 전체 갯수 
-    for(i in 0 until n){
-        for(j in 0 until n){
-            answer += trees[i][j].size
-        }
-    }
-    print(answer)
+    trees = PriorityQueue(survive)
 }
-
-fun isInBound(x: Int, y: Int): Boolean{
-    return (x in 0 until n) && (y in until n)
-}
+data class Tree(val r: Int, val c: Int, val lev: Int) : Comparable<Tree> {
+    override fun compareTo(other: Tree) : Int = lev - other.lev
+ }
